94b9eb9cce2906975551d2c26694a2c9d3ee1f43
Tram Doan <37705231+TramD@users.noreply.github.com>
2019-10-09 17:24:35 -0500

    edited server.py
    This is the server.py that I used to test the client.py if you guys wanted to try it.

--- server.py
+++ server.py
@@ -1,49 +1,44 @@
+# import Python's libraries
 import socket
-from time import sleep
+import time
 from binascii import hexlify
 
-ZERO = 0.025
-ONE = 0.1
-PORT = 1337
+# create a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", PORT))
 
+# bind the socket to all interfaces on some port
+port = 1337
+s.bind(("", port))
 
-covert = "THIS IS A SECRET THING " + "EOF"
-print("Covert message: " + covert)
-covert_bin = ""
-for i in covert:
-    covert_bin += bin(int(hexlify(i.encode()), 16))[2:].zfill(8)
-    
-print("Covert in binary: " + covert_bin)
-n = 0
-bin = ""
-
-# listens for clients
-# this is a blocking call
+# listen for connections:
 s.listen(0)
 
-# a client has connected!
-c,addr = s.accept()
+# accept a connecting client, addr is address of connecting client
+c, addr = s.accept()
 
-# set the message
-msg = "Some message..."
+# set one and zero timings
+ZERO = 0.025
+ONE = 0.1
 
-# send the message, one letter at a time
-print("Sending characters with delays:")
+msg = "Some message message message message message message message message ..."
 n = 0
-count = 0
-while(count < len(covert_bin)):
-    for i in msg:
-        c.send(i.encode())
-        if (covert_bin[n] == "0"):
-            sleep(ZERO)
-        else:
-            sleep(ONE)
-        n = (n + 1) % len(covert_bin)
-        count += 1
-        
+
+# convert message to binary
+covert = "secret" + "EOF"
+covert_bin = ""
+for i in covert:
+    # convert each character to a full byte
+    covert_bin += bin(int(hexlify(i.encode()), 16))[2:].zfill(8)
+
+# send a message, one character at a time with a delay in between characters:
+for i in msg:
+    c.send(i.encode())
+    if (covert_bin[n] == "0"):
+        time.sleep(ZERO)
+    else:
+        time.sleep(ONE)
+    n = (n + 1) % len(covert_bin)
 c.send("EOF".encode())
+
+# close the connection
 c.close()
-
-
